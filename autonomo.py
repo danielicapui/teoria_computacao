aa6e3628bb869ff1638c6891a8191fe3d371343a
danielicapui <37754709+danielicapui@users.noreply.github.com>
2021-03-10 18:24:01 -0300

    Adicionando um AFND incompleto
    Falta finalizar o inpercurso. Bom, o algoritmo irá funcionar como afd e talvez se alterar um pouco os testes ele fará o essencial para resolver o problema
    Tente percorrer somente as ligações verificando.

--- autonomo.py
+++ autonomo.py
@@ -2,9 +2,8 @@ class Estado:
     def __init__(self,inicial,final):
         self.inicial=inicial
         self.final=final
-
     def reacoes(self):
-        print("As ligações são no formato estado_atual entrada estado_de_destino, exemplo (0,1,1) para lambda digite 666")
+        print("As ligações são no formato estado_atual,entrada,estado_de_destino exemplo (0,1,1) para lambda digite 666")
         r=int(input("Diga quantas ligações este estado possuí:"))
         self.inu=[]
         for i in range(r):
@@ -13,15 +12,31 @@ class Estado:
             self.inu.append(e)
     def mostrarReacoes(self):
         print(self.inu)
+    def inreacoes(self):
+        print("As ligações são no formato estado_atual,entrada,estado_de_destino exemplo (0,1,1) para lambda digite 666")
+        r=int(input("Diga quantas ligações este estado possuí:"))
+        self.afnd=[]
+        self.lambdas=[]
+        self.pos=[]
+        for i in range(r):
+            print("Digite a ligação {} do estado:".format(i))
+            e=input().split(",")
+            if e[1]=="666":
+                self.lambdas.append(e)
+                self.pos.append(i)
+            self.afnd.append(e)
+        
 class Linguagem:
     def __init__(self,palavra):
         self.palavra=palavra
 
 class Teste:
-    def __init__(self,estados,ini,finais):
+    def __init__(self,estados,ini,finais,lambdas,posicao):
         self.estados=estados
         self.ini=ini
         self.finais=finais
+        self.lam=lambdas
+        self.posicao=posicao
     def Cadeia(self):
         cadeia=input("Digite a cadeia")
         self.entradas=[]
@@ -48,8 +63,29 @@ class Teste:
                 if self.verificar()==True:
                     break
             print("q{}".format(self.atual))
-                 
-            
+    def inpercorrer(self):             
+        self.atual=self.ini
+        self.atuais=[self.ini]
+        found=False
+        caminhos=[]
+        for i in range(len(self.entradas)):
+            for q in range(len(self.estados)):  
+                
+                for c in range(len(self.estados)):
+                    if self.estados[q].afnd[c][1]==self.entrada[i]:
+                        if self.atual==self.estados[q].afnd[c][0]:
+                            caminhos.append(c)
+                    elif self.atual==self.lam[self.posicao]:
+                        print("Chegou aqui")
+            if found==False:
+                print("Cadeia não aceita")
+                break
+            if i!=len(self.entradas)-1:
+                found=False
+            if i==len(self.entradas)-1:
+                if self.verificar()==True:
+                    break
+            print("q{}".format(self.atual))
     def verificar(self):
         for i in self.finais:
             if str(self.atual)==str(i):
@@ -60,9 +96,10 @@ class Teste:
         return False
 def adicionarLinguagem():
     print("As linguagens devem ser no formato minusculas. Exemplo de linguagem: a bn n>0 ")
+    #a bn n>1
+    print("Em desenvolvimento no futuro.")
     regra=input("Qual a regra?").split()
-    for i in range(len(regra)):
-        regra[i]==entrada
+    
 
 
 def criarAutonomo(estado_inicial):
@@ -71,6 +108,7 @@ def criarAutonomo(estado_inicial):
     ei=9
     estados_finais=[]
     for i in range(n):
+        print("Estado q{}".format(i))
         es=int(input("Este é um estado final?;0 ou 1\n"))
         if es==1:
             estados_finais.append(i)
@@ -83,29 +121,68 @@ def criarAutonomo(estado_inicial):
         shika=Estado(ei,es)
         shika.reacoes()
         estado.append(shika)
+        print("Estado armazenado!")
     return estado,estado_inicial,estados_finais
 
 
+def criarAFND(estado_inicial):
+    n=int(input("Quantos estados tem nesse AFND?:\n"))
+    estado=[]
+    ei=9
+    estados_finais=[]
+    la=[]
+    p=[]
+    for i in range(n):
+        print("Estado q{}".format(i))
+        es=int(input("Este é um estado final?;0 ou 1\n"))
+        if es==1:
+            estados_finais.append(i)
+        if estado_inicial==False:
+            ei=int(input("Este é um estado inicial?\n"))
+            if ei==1:
+                estado_inicial=str(i)
+        else:
+            ei=0
+        shika=Estado(ei,es)
+        shika.inreacoes()
+        la.append(shika.lambdas)
+        p.append(shika.pos)
+        estado.append(shika)
+        print("Estado armazenado!")
+    return estado,estado_inicial,estados_finais,p,la
 def Interface():
     print("Bem-vindo a interface. 0==false, 1==true")
     estados=0
     estado_inicial=False
     linguagem=False
+    p=0
+    lambdas=0
     while(True):
         op=int(input("Digite 1 para digitar uma AFD;\nDigite 2 para digitar um AFND;\nDigite 666 para encerrar o programa;\nDigite 3 para adicionar uma linguagem;\nDigite 4 para testar o autônomo AFD;\nDigite 5 para testar o autônomo AFND;\nDigite 6 para mostrar o autônomo;\nDigite 7 para limpar os autônomos;\n"))
         if op==1:
             estados,estado_inicial,estados_finais=criarAutonomo(estado_inicial)
         elif op==2:
-            pass
+            estados,esta_inicial,estados_finais,p,lambdas=criarAFND(estado_inicial)
         elif op==666:
             break
         elif op==3:
             print("Em desenvolvimento.")
             #adicionarLinguagem()
         elif op==4:
-            teste=Teste(estados,estado_inicial,estados_finais)
+            teste=Teste(estados,estado_inicial,estados_finais,lambdas,p)
             teste.Cadeia()
             teste.percorrer()
+        elif op==5:
+            print("Em desenvolvimento de excessões, cadeias extensão são serão aceitas devido a limitação de tempo")
+            teste.Teste(estados,estado_inicial,estados_finais,lambdas,p)
+        elif op==6:
+            print(estados)
+        elif op==7:
+            estados=0
+            estado_inicial=False
+            linguagem=False
+            print("Limpeza conclúída.")
+        
             
 def main():
     Interface()
